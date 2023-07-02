0932cd2e770eeb38860a37c828a3858af9216b49
MHarmony <mharmony@mharmony.io>
2023-07-01 22:05:09 -0400

    refactor(ui): add sentry to ui
    basic setup

--- apps/ui/src/app/app.config.ts
+++ apps/ui/src/app/app.config.ts
@@ -1,10 +1,16 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  ErrorHandler,
+} from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {
+  Router,
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
+import { TraceService, createErrorHandler } from '@sentry/angular-ivy';
 import { appRoutes } from './app.routes';
 
 /**
@@ -15,5 +21,23 @@ export const appConfig: ApplicationConfig = {
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideAnimations(),
     provideHttpClient(),
+    {
+      provide: ErrorHandler,
+      useValue: createErrorHandler({
+        showDialog: true,
+      }),
+    },
+    {
+      provide: TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {
+        return;
+      },
+      deps: [TraceService],
+      multi: true,
+    },
   ],
 };
--- apps/ui/src/main.ts
+++ apps/ui/src/main.ts
@@ -1,7 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import {
+  BrowserTracing,
+  init,
+  instrumentAngularRouting,
+} from '@sentry/angular-ivy';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+
+init({
+  dsn: process.env['SENTRY_DSN'],
+  integrations: [
+    new BrowserTracing({
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      shouldCreateSpanForRequest: (url: string) => true,
+      tracePropagationTargets: ['localhost'],
+      routingInstrumentation: instrumentAngularRouting,
+    }),
+  ],
+  tracesSampleRate: 1,
+});
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
+  console.error(err),
 );
